51d2211612ac7d817cef9a19cd13429f6ec8813e
sareteni <68252941+sareteni@users.noreply.github.com>
2023-06-03 20:00:13 -0700

    Add files via upload

---
+++ audio.ino
@@ -0,0 +1,49 @@
+/***************************************************
+DFPlayer - A Mini MP3 Player For Arduino
+ <https://www.dfrobot.com/product-1121.html>
+ 
+ ***************************************************
+ This example shows the basic function of library for DFPlayer.
+ 
+ Created 2016-12-07
+ By [Angelo qiao](Angelo.qiao@dfrobot.com)
+ 
+ GNU Lesser General Public License.
+ See <http://www.gnu.org/licenses/> for details.
+ All above must be included in any redistribution
+ ****************************************************/
+
+/***********Notice and Trouble shooting***************
+ 1.Connection and Diagram can be found here
+ <https://www.dfrobot.com/wiki/index.php/DFPlayer_Mini_SKU:DFR0299#Connection_Diagram>
+ 2.This code is tested on Arduino Uno, Leonardo, Mega boards.
+ ****************************************************/
+
+#include "Arduino.h"
+#include "SoftwareSerial.h"
+#include "DFRobotDFPlayerMini.h"
+
+SoftwareSerial mySoftwareSerial(10, 11); // RX, TX
+DFRobotDFPlayerMini myDFPlayer;
+
+
+void setup()
+{
+  
+  myDFPlayer.volume(22);  //Set volume value. From 0 to 30
+ 
+}
+
+void loop()
+{
+  
+    myDFPlayer.play(1);
+    delay (20000);
+  }
+  
+  
+
+
+
+  
+  
